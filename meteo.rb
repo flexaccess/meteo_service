4545065aa3da05af128d5d06f91b66952de3ea56
Mirka <flexaccess@hotmail.com>
2017-08-30 01:40:43 +0300

    Version 2.0 - add dictionary of synonyms parser, fix bags and new input words

--- meteo.rb
+++ meteo.rb
@@ -3,6 +3,10 @@ require 'net/http' # send request on websites
 require 'uri' # format address
 require 'rexml/document' # parse xml docs
 require 'json' # parse json docs
+require 'open-uri'
+require 'nokogiri'
+require 'unicode'
+require_relative 'unicode-settings.rb'
 
 CLOUDINESS = {-1 => 'туман', 0 => 'ясно', 1 => 'малооблачно', 2 => 'облачно', 3 => 'пасмурно'}
 
@@ -23,7 +27,33 @@ user_input = STDIN.gets.chomp
 city_key = nil
 
 cities_hash.each do |key, values|
-  city_key = key if values == user_input
+  city_key = key if values.capitalize == user_input.capitalize
+end
+
+if city_key.nil?
+  first_big_word = user_input.split("")[0].upcase
+  word_downcase = user_input.downcase
+
+  url_sin = "http://synonymonline.ru/#{first_big_word}/#{word_downcase}"
+  url_sin = URI.escape(url_sin)
+
+  begin
+    html = open(url_sin)
+    doc_sin = Nokogiri::HTML(html)
+
+    synonyms = []
+
+    doc_sin.css('.synonyms-list li span').each do |list|
+      synonyms << list.text
+    end
+
+    cities_hash.each do |key, values|
+      synonyms.each do |value|
+        city_key = key if values.capitalize == value.capitalize
+      end
+    end
+  rescue OpenURI::HTTPError
+  end
 end
 
 city_key = cities_hash.keys[0] if city_key.nil?
@@ -52,7 +82,7 @@ word_end = word_split[-1]
 if (word_end == "а" || word_end == "ь" || word_end == "я")
   word_split.delete(word_end)
   word_split << "е"
-elsif (word_end == "ы" || word_end == "о" || word_end == "у" || word_end == "и")
+elsif (word_end == "ы" || word_end == "о" || word_end == "у" || word_end == "и" || word_end == "й")
 
 else
   word_split << "е"
